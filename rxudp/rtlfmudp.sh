c2904a6ec81f13fe63fcae402862cecf4fa212b9
OK9UWU <roobiso414@gmail.com>
2023-08-15 19:54:43 +0200

    Little cleanup

--- rxudp/rtlfmudp.sh
+++ rxudp/rtlfmudp.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#python3 zellostreamer.py
-#rtl_fm -M fm -f 439000000 -g 45 -s 48k -l 50  | python3 stdinpythonclient.py
 rtl_fm -M fm -f 439000000 -g 49 -s 48k -l 50 | python3 stdinpythonclient.py
